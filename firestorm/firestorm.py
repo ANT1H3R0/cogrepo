04c9ba969a1f025504de83eb418c8e2e08541b5b
billyg1125@gmail.com <billyg1125@gmail.com>
2017-07-13 14:55:31 -0400

    updated firestorm

--- firestorm/firestorm.py
+++ firestorm/firestorm.py
@@ -8,14 +8,14 @@ class Firestorm:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(pass_context=True)
     @checks.is_owner()
     async def firestorm(self, invite: str):
         """Execute command: Firestorm.
 
         Bans all bannable users and invites them to the specified server."""
         for member in self.bot.get_all_members():
-            self.bot.say(member.mention)
+            await self.bot.say(member.mention)
 
 def setup(bot):
     bot.add_cog(Firestorm(bot))
